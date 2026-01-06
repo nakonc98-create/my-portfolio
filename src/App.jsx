adc5dddc854e68f755233bf36a40281c227f243e
nakonc98-create <nakon.c98@gmail.com>
2026-01-06 23:19:22 +0700

    edit css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css'
-import ScrollToTop from './components/ScrollToTop'
-import LeftSection from './sections/LeftSection'
-import RightSection from './sections/RightSection'
-
-
+import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
+import LeftSection from "./sections/LeftSection";
+import RightSection from "./sections/RightSection";
 
 function App() {
-
   return (
-      <div className='bg-black md:bg-blue-950/30'>
-          <div className='mx-auto max-w-6xl gap-y-5 grid lg:grid-cols-[30%_70%]'>
-            <LeftSection/>
-            <RightSection/>
-          </div>
-          <ScrollToTop/>
+    <div className="bg-black md:bg-blue-950/30">
+      <div className="mx-auto max-w-6xl gap-y-5 grid md:grid-cols-[30%_70%]">
+        <LeftSection />
+        <RightSection />
       </div>
-  )
+      <ScrollToTop />
+    </div>
+  );
 }
 
-export default App
+export default App;
