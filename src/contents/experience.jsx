0254fbbe6536eedd9fc37e0109d1eeaa1e97ce06
nakonc98-create <nakon.c98@gmail.com>
2026-02-13 22:36:52 +0700

    Edit description

--- src/contents/experience.jsx
+++ src/contents/experience.jsx
@@ -10,7 +10,7 @@ export const data = [
       "คณะสัตวแพทยศาสตร์ มหาวิทยาลัยสงขลานครินทร์ - ฝึกงานตำแหน่ง IT Support",
     duration: "17 เม.ย. 2566 - 12 มิ.ย. 2566",
     description:
-      "        ฝึกงานตำแหน่งไอทีซัพพอร์ตโดยใช้เครื่องมือของ Microsoft Office 365 อย่างเช่น Microsoft Power Apps , Microsoft Power Automate , Microsoft Power BI , Microsoft List , Microsoft Sharepoint เพื่อผลิตซอฟต์แวร์ช่วยงานตรวจสอบตู้ไฟฟ้าที่ชํารุดภายในอาคาร",
+      "ฝึกงานตำแหน่งไอทีซัพพอร์ตโดยใช้เครื่องมือของ Microsoft Office 365 อย่างเช่น Microsoft Power Apps , Microsoft Power Automate , Microsoft Power BI , Microsoft List , Microsoft Sharepoint เพื่อพัฒนาแอปพลิเคชันสำหรับตรวจสอบตู้ไฟฟ้าภายในอาคารด้วย Microsoft Power Apps ช่วยให้เจ้าหน้าที่บันทึกข้อมูลได้รวดเร็วและลดการใช้กระดาษ (Paperless)",
     skill: [
       "Microsoft Power App",
       "Microsoft Power Automate",
