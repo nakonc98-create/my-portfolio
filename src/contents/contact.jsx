42a1207aed35c6c482eeccc99c24727f82dfc56a
nakonc98-create <nakon.c98@gmail.com>
2026-02-16 09:30:42 +0700

    Edit Footer and Update Link Facebook

--- src/contents/contact.jsx
+++ src/contents/contact.jsx
@@ -1,22 +1,26 @@
-import { faFacebook, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const data = [
-    {
-        id: 1,
-        name: "GitHub",
-        link: "https://github.com/nakonc98-create",
-        icon: faGithub,
-    },
-    {
-        id: 2,
-        name: "Instagram",
-        link: null,
-        icon: faInstagram,
-    },
-    {
-        id: 3,
-        name: "Facebook",
-        link: null,
-        icon: faFacebook,
-    },
+  {
+    id: 1,
+    name: "GitHub",
+    link: "https://github.com/nakonc98-create",
+    icon: faGithub,
+  },
+  {
+    id: 2,
+    name: "Instagram",
+    link: null,
+    icon: faInstagram,
+  },
+  {
+    id: 3,
+    name: "Facebook",
+    link: "https://www.facebook.com/mas.s.508229",
+    icon: faFacebook,
+  },
 ];
