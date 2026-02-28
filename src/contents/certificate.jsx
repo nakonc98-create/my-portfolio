c9b63e80551ed9052d81bc80cc9614ddd22a8589
nakonc98-create <nakon.c98@gmail.com>
2026-02-28 15:56:45 +0700

    Add Cert

--- src/contents/certificate.jsx
+++ src/contents/certificate.jsx
@@ -10,6 +10,22 @@ export const data = [
   },
   {
     id: 2,
+    title: "Learn the Command Line",
+    institution: "Codecademy",
+    year: "2026",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Learn%20the%20Command%20Line_Codecademy.jpg?alt=media&token=27c69fb5-03a2-4de3-b6d6-787b23f02129",
+  },
+  {
+    id: 3,
+    title: "Fundamentals of Operating Systems",
+    institution: "Codecademy",
+    year: "2026",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Fundamentals%20of%20Operating%20Systems_Codecademy.jpg?alt=media&token=242aceda-a82a-4aa3-b120-8fe2dea70866",
+  },
+  {
+    id: 4,
     title: "Introduction to Linux",
     institution: "Codecademy",
     year: "2025",
@@ -17,7 +33,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Introduction%20to%20Linux_Codecademy.jpg?alt=media&token=4beb830a-dca9-4a55-b279-4b63407c6b11",
   },
   {
-    id: 3,
+    id: 5,
     title: "Introduction to Linux: Installation",
     institution: "Codecademy",
     year: "2025",
@@ -25,7 +41,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Introduction%20to%20Linux%20-%20Installation_Codecademy.jpg?alt=media&token=0301097e-56c2-4533-8367-fb181089211f",
   },
   {
-    id: 4,
+    id: 6,
     title: "Introduction to Linux: Bash Scripting",
     institution: "Codecademy",
     year: "2025",
@@ -33,7 +49,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Introduction%20to%20Linux%20Bash%20Scripting_Codecademy.jpg?alt=media&token=0bcf5da7-4c77-41c7-abf9-900a3701dcd3",
   },
   {
-    id: 5,
+    id: 7,
     title: "Introduction to Linux: Users and Permissions",
     institution: "Codecademy",
     year: "2025",
@@ -41,7 +57,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Introduction%20to%20Linux%20Users%20and%20Permissions_Codecademy.jpg?alt=media&token=a59fadcb-a896-4115-8a36-021db6c70649",
   },
   {
-    id: 6,
+    id: 8,
     title: "Intro to Cloud Computing",
     institution: "Codecademy",
     year: "2025",
@@ -49,7 +65,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Intro%20to%20Cloud%20Computing_Codecademy.jpg?alt=media&token=214426c1-abad-40a5-9705-3e1fd58e5eb9",
   },
   {
-    id: 7,
+    id: 9,
     title: "Operating Systems: Introduction",
     institution: "Codecademy",
     year: "2025",
@@ -57,7 +73,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Cert_Operating%20Systems%20Introduction_Codecademy.jpg?alt=media&token=668d1b5e-f724-4a1b-bfb7-462c598a6c9d",
   },
   {
-    id: 8,
+    id: 10,
     title: "Computer Hardware",
     institution: "PSUMOOC",
     year: "2025",
@@ -65,7 +81,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Certificate_Computer_Hardware.jpg?alt=media&token=e783053e-9fb6-426d-95ba-6cbe2c4da48e",
   },
   {
-    id: 9,
+    id: 11,
     title: "Computer Software",
     institution: "PSUMOOC",
     year: "2025",
@@ -73,7 +89,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Certificate_Computer_Software.jpg?alt=media&token=54045c25-711f-4fce-8e7d-44919323ab8c",
   },
   {
-    id: 10,
+    id: 12,
     title: "Basic Computer Security",
     institution: "PSUMOOC",
     year: "2025",
@@ -81,7 +97,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Certificate_Basic_Computer_Security.jpg?alt=media&token=4fec921d-16a2-4da2-ab19-041bdfd5d315",
   },
   {
-    id: 11,
+    id: 13,
     title: "Career of IT",
     institution: "PSUMOOC",
     year: "2025",
@@ -89,7 +105,7 @@ export const data = [
       "https://firebasestorage.googleapis.com/v0/b/my-certificate-f261a.firebasestorage.app/o/e-Certificate_Career_of_IT.jpg?alt=media&token=ec3e97d9-59cc-485c-b788-5b677bad2260",
   },
   {
-    id: 12,
+    id: 14,
     title: "Evolution Computer",
     institution: "PSUMOOC",
     year: "2025",
