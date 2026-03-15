4a31f59087c39e1373feea8f87316e9f405c40a3
nakonc98-create <nakon.c98@gmail.com>
2026-03-16 02:51:11 +0700

    Edit Scale

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,18 +1,30 @@
-import { faFacebook, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { data } from '../contents/contact'
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { data } from "../contents/contact";
 
 const Contact = () => {
   return (
-    <div className='flex items-end justify-center md:justify-normal text-2xl gap-4 mt-10'>
-      {data.map((e,i) => (
-        <a key={i} href={e.link} target="_blank" rel="noopener noreferrer" aria-label={e.name}>
-          <FontAwesomeIcon className={`transition-all duration-300 ${e.link ? "hover:scale-125 hover:text-blue-500 cursor-pointer": "opacity-40 pointer-events-none cursor-none"}`} 
-          icon={e.icon}/>
+    <div className="flex items-end justify-center md:justify-normal text-2xl gap-4 mt-4">
+      {data.map((e, i) => (
+        <a
+          key={i}
+          href={e.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={e.name}
+        >
+          <FontAwesomeIcon
+            className={`transition-all duration-300 ${e.link ? "hover:scale-125 hover:text-blue-500 cursor-pointer" : "opacity-40 pointer-events-none cursor-none"}`}
+            icon={e.icon}
+          />
         </a>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/sections/LeftSection.jsx
+++ src/sections/LeftSection.jsx
@@ -6,7 +6,7 @@ import Contact from "./Contact";
 const LeftSection = ({ headerData }) => {
   return (
     <div className="px-7 md:bg-black">
-      <div className="sticky top-14 grid gap-y-5 lg:grid-rows-[2fr_2fr_1fr] lg:h-[90vh]">
+      <div className="sticky top-5 grid gap-y-5 md:grid-rows-[2fr_2fr_1fr] md:h-[90vh]">
         <Header data={headerData} />
         <Navbar />
         <Contact />
